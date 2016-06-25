00089c3ebfdbec5d66c889c493c11f39d527d4aa
Leo SantAnna <leonardoebs@gmail.com>
2016-06-24 23:04:35 -0300

    Added variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Leo',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Leo',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [ 15, 27 ];
+
+function addGrade(array) {
+	//array = [15, 27, 45];
+	array.push(50);
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
