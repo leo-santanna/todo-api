5877ca441c1f4f01062eb35e4db1f3e63d404033
Leo SantAnna <leonardoebs@gmail.com>
2016-06-29 12:45:27 -0300

    Converted DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -69,18 +69,22 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchingTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (matchingTodo) {
-		todos = _.without(todos, matchingTodo);
-		res.json(matchingTodo);
-	} else {
-		res.status(404).json({
-			"error": "No Todo found with given id"
-		});
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if(rowsDeleted > 0) {
+			res.status(204).send();
+		} else {
+			res.status(404).json({
+				error: 'No todo found with given id!'
+			});
+		}
+	}).catch(function(e) {
+		res.status(500).send();
+	});
 });
 
 // PUT /todos/:id
