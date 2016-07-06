613980dacc0a52aa0ec5818e0e024783d29d2939
Leo SantAnna <leonardoebs@gmail.com>
2016-07-05 21:34:48 -0300

    Added Hash and Salt to user password

--- server.js
+++ server.js
@@ -120,14 +120,14 @@ app.put('/todos/:id', function(req, res) {
 app.post('/users', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
-	db.user.create(body).then(function (todo) {
-		res.json(todo.toPublicJSON());
+	db.user.create(body).then(function (user) {
+		res.json(user.toPublicJSON());
 	}).catch(function (e) {
 		res.status(400).json(e);
 	});
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
